2fb55589a96784f39f40e7be5947a69d4e235fe9
Arda Şalvarlılar <arda4salvar7@gmail.com>
2024-02-11 11:18:17 +0300

    sequelize connection

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const config = {
     host: "localhost",
     user: "root",
     password: "admin123",
-    database: "blogapp"
+    database: "blogdb"
   }
 }
 
--- data/db.js
+++ data/db.js
@@ -1,14 +1,19 @@
 const mysql = require("mysql2")
 const config = require('../config')
 
-let connection = mysql.createConnection(config.db)
-
-connection.connect((err) => {
-  if(err){
-    return console.log(err)
-  }
-
-  console.log('mysql server connected')
+const {Sequelize} = require('sequelize')
+const sequelize = new Sequelize(config.db.database,config.db.user,config.db.password,{
+  dialect: 'mysql',
+  host: config.db.host
 })
+async function connect(){
+  try {
+    await sequelize.authenticate();
+    console.log('connected to mysql')
+  } catch (error) {
+    console.log(error)
+  }
+}
+connect()
 
-module.exports = connection.promise()
+module.exports = sequelize
