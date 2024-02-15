3fc2847b334e9f9eaa14fb9663031581074007b5
Arda Şalvarlılar <arda4salvar7@gmail.com>
2024-02-15 17:21:06 +0300

    sending email for password reset

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const User = require('../models/user')
 const bcrypt = require('bcrypt')
 const emailService = require('../helpers/send-mail')
 const config = require('../config')
+const crypto = require('crypto')
 
 exports.get_register = async (req,res) => {
   try {
@@ -96,6 +97,52 @@ exports.get_logout = async (req,res) => {
   try {
     await req.session.destroy()
     return res.redirect('/account/login')
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+exports.get_reset = async (req,res) => {
+  const message = req.session.message
+  delete req.session.message
+  try {
+    return res.render('auth/reset-password', {
+      title: 'reset password',
+      message: message
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+exports.post_reset = async (req,res) => {
+  const email = req.body.email
+  try {
+    let token = crypto.randomBytes(32).toString('hex')
+    const user = await User.findOne({where: {email: email}})
+
+    if(!user){
+      req.session.message = {text: 'Girdiğiniz mail adresi bulunamadı', class: 'danger'}
+      return res.redirect('reset-password')
+    }
+
+    user.resetToken = token
+    user.resetTokenExpiration = Date.now() + (1000 * 60 * 60)
+    await user.save()
+
+    emailService.sendMail({
+      from: config.email.from,
+      to: email,
+      subject: 'reset password',
+      html: `
+        <p>Parolanızı günvellemek için aşağıdaki linki kullanın</p>
+        <p>
+          <a href="http://127.0.0.1:3000/account/reset-password/${token}">Parola sıfırla</a>
+        </p>
+      `
+    })
+    req.session.message = {text: 'Parolanızı sıfırlamak için eposta adresinizi kontrol ediniz', class:'success'}
+    res.redirect('login')
   } catch (error) {
     console.log(error)
   }
